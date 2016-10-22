7a73fe737500da00341b44cabcbedb60ddd1c277
Antonio Ortiz <tony.ortiz@xamarinos.com>
2016-10-22 18:37:52 -0500

    Update - AdMob for UWP

---
+++ PlugIn/Xamarinos.AdMob.Forms/Xamarinos.AdMob.Forms.UWP/AdBannerRenderer.cs
@@ -0,0 +1,20 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+using Xamarinos.AdMob.Forms.UWP;
+
+[assembly: ExportRenderer(typeof(Xamarinos.AdMob.Forms.AdBanner), typeof(AdBannerRenderer))]
+namespace Xamarinos.AdMob.Forms.UWP
+{
+    public class AdBannerRenderer : ViewRenderer<StackLayout, Windows.UI.Xaml.Controls.StackPanel>
+    {
+        public static void Init()
+        {
+        }
+
+        public AdBannerRenderer()
+        {
+
+        }
+    }
+}
---
+++ PlugIn/Xamarinos.AdMob.Forms/Xamarinos.AdMob.Forms.UWP/AdsInterstitialImplementation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarinos.AdMob.Forms.Abstractions;
+
+namespace Xamarinos.AdMob.Forms
+{
+    public class AdsInterstitialImplementation : IInterstitialAdManager
+    {
+        public AdsInterstitialImplementation()
+        {
+        }
+
+        public async Task Show(Action OnPresented = null)
+        {
+            await Task.Delay(1);
+        }
+    }
+}
